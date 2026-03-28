a5f36604ba6c297dce1fff6e7dce3c801e4d07f0
Bakhtier Ravshanovich Abdullaev <bakhtierabdullaev@mailbox.unideb.hu>
2026-03-28 08:54:27 +0100

    feat: add working Telegram bot

---
+++ telegram-bot.py
@@ -0,0 +1,44 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+from brain import agent_executor
+load_dotenv
+BOT_TOKEN=os.getenv('TELEGRAM_BOT_TOKEN')
+
+if not BOT_TOKEN:
+    print("ERROR: Could not find TELEGRAM_BOT_TOKEN in .env file!")
+    exit()
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    welcome_text = "✈️ **Welcome to Atlas Travel Agent!** \n\nTell me where you want to go. (e.g., 'Find me flights from Budapest to Barcelona on 2026-05-14')"
+    bot.reply_to(message, welcome_text, parse_mode="Markdown")
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    print(f"\n--> Received from {message.from_user.first_name}: {message.text}")
+    bot.send_chat_action(message.chat.id, 'typing')
+    try:
+        # passing user message to model
+        response = agent_executor.invoke({"messages": [("user", message.text)]})
+        
+        #text cleaning
+        raw_content = response["messages"][-1].content
+        if isinstance(raw_content, list):
+            clean_text = raw_content[0]['text']
+        else:
+            clean_text = raw_content
+            
+        bot.reply_to(message, clean_text, parse_mode="Markdown")        
+        print("--> Replied successfully!")
+        
+    except Exception as e:
+        bot.reply_to(message, "Sorry, I am having trouble connecting to my systems. Please try again!")
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    print("🚀 Telegram Bot is online and listening! Open Telegram and say hi.")
+    bot.infinity_polling()
